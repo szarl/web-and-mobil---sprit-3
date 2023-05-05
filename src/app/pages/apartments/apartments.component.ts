c7b83b2f72db2784438538cec65f196bc626b461
gorazdKuzev12 <gorazd.kuzev10@gmail.com>
2023-05-05 18:45:00 +0200

    feat: Dynamicly apartmant

--- src/app/pages/apartments/apartments.component.ts
+++ src/app/pages/apartments/apartments.component.ts
@@ -1,34 +1,42 @@
-import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-apartments',
   templateUrl: './apartments.component.html',
-  styleUrls: ['./apartments.component.scss']
+  styleUrls: ['./apartments.component.scss'],
 })
-export class ApartmentsComponent {
-  images = [
-    {
-      path: 'assets/apartments/apartment-1.jpg',
-      alt: 'Apartment 1'
-    },
-    {
-      path: 'assets/apartments/apartment-2.jpg',
-      alt: 'Apartment 2'
-    },
-    {
-      path: 'assets/apartments/apartment-3.jpg',
-      alt: 'Apartment 3'
-    },
-    {
-      path: 'assets/apartments/apartment-4.jpg',
-      alt: 'Apartment 4'
-    },
-    {
-      path: 'assets/apartments/apartment-5.jpg',
-      alt: 'Apartment 5'
-    },
-    {
-      path: 'assets/apartments/apartment-6.jpg',
-      alt: 'Apartment 6'
-    },
-  ]}
+export class ApartmentsComponent implements OnInit {
+  apartments: any[] = [];
+  displayedApartments: any[] = [];
+  currentPage: number = 1;
+  pageSize: number = 6;
+
+  constructor(private db: AngularFireDatabase) {}
+
+  ngOnInit() {
+    this.db
+      .list('apartments')
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({
+            key: c.payload.key,
+            ...(c.payload.val() as any),
+          }))
+        )
+      )
+      .subscribe((apartments) => {
+        this.apartments = apartments;
+        this.displayedApartments = apartments.slice(0, this.pageSize);
+      });
+  }
+
+  changePage(newPage: number) {
+    const startIndex = (newPage - 1) * this.pageSize;
+    const endIndex = newPage * this.pageSize;
+    this.currentPage = newPage;
+    this.displayedApartments = this.apartments.slice(startIndex, endIndex);
+  }
+}
